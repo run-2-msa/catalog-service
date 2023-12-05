1bcf41fd540d93ae5b1be0eed703d42cca20f87e
Develing1991 <completed0728@gmail.com>
2023-12-05 12:05:57 +0900

    refactor: add All, No Args constructor annotation

--- src/main/java/com/example/catalogservice/vo/CatalogResponse.java
+++ src/main/java/com/example/catalogservice/vo/CatalogResponse.java
@@ -1,13 +1,20 @@
 package com.example.catalogservice.vo;
 
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@JsonNaming(value = PropertyNamingStrategies.SnakeCaseStrategy.class)
 public class CatalogResponse {
     private String productId;
     private String productName;
